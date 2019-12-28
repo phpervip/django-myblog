a141c1a507c46b938a62a80a22c98bab887709c8
phpervip <2998658517@qq.com>
2019-12-28 22:06:39 +0800

    Django模板中的超链接配置

--- myblog/urls.py
+++ myblog/urls.py
@@ -22,6 +22,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls', namespace='blog')),
-    url(r'^blog2/', include('blog2.urls', namespace='blog2'))
+    url(r'^blog/', include(('blog.urls', 'blog'), namespace='blog')),
+    url(r'^blog2/', include(('blog2.urls', 'blog2'), namespace='blog2'))
 ]

e9e749bdabd9151c403ee5ec1f49f5487ac0adff
phpervip <2998658517@qq.com>
2019-12-29 19:35:15 +0800

    修改页面样式

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,6 @@ from blog.models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'pub_time')
 
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 
 

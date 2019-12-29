81ba6e3d01c7bddc7a404eab3771b8310d1f5bcf
phpervip <2998658517@qq.com>
2019-12-29 14:19:20 +0800

    完成

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 from blog.models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'datetime')
+
 admin.site.register(Article)
 
 
